807c772a7d4a2919b477e15d53913e70f767b648
Max <biryukov.mm@gmail.com>
2024-01-24 11:47:43 +0200

    feat: Add backend scripts

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,15 +1,16 @@
-import express from 'express';
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const app = express();
+const app: Express = express();
 const port = process.env.PORT || 4000;
 
-app.get('/', (_req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
